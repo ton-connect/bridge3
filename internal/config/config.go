9fbf4ad54987dcfa30b96de956493d11e7afc77a
callmedenchick <d.volkov@top.team>
2025-07-09 18:59:36 +0200

    increase limits

--- internal/config/config.go
+++ internal/config/config.go
@@ -13,9 +13,9 @@ var Config = struct {
 	CopyToURL             string   `env:"COPY_TO_URL"`
 	CorsEnable            bool     `env:"CORS_ENABLE"`
 	HeartbeatInterval     int      `env:"HEARTBEAT_INTERVAL" envDefault:"10"`
-	RPSLimit              int      `env:"RPS_LIMIT" envDefault:"100"`
+	RPSLimit              int      `env:"RPS_LIMIT" envDefault:"1000"`
 	RateLimitsByPassToken []string `env:"RATE_LIMITS_BY_PASS_TOKEN"`
-	ConnectionsLimit      int      `env:"CONNECTIONS_LIMIT" envDefault:"50"`
+	ConnectionsLimit      int      `env:"CONNECTIONS_LIMIT" envDefault:"200"`
 	SelfSignedTLS         bool     `env:"SELF_SIGNED_TLS" envDefault:"false"`
 }{}
 
